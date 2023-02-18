0db9e24dfd1ce739800a59c0341787e444dde9a5
Olivier Mathurin <o.mathfly@gmail.com>
2023-02-18 18:17:08 +0100

    [remove]unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { step } from 'types';
-import './App.css';
+import type { step } from 'types';
+
+import { Col, Container, Row } from 'react-bootstrap';
 import Step1 from './components/Step1';
+import Step2 from './components/Step2';
+import Step3 from './components/Step3';
 import Stepper from './components/Stepper';
+
+// CSS IMAGES
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import bgV from './assets/images/bg-sidebar-desktop.svg';
 
 const stepData: step[] = [
   {
@@ -15,14 +23,14 @@ const stepData: step[] = [
     type: 'STEP',
     title: 'Select your plan',
     resume: 'You have the option of monthly or yearly billing.',
-    component: <div>step2</div>,
+    component: <Step2 />,
     buttonTag: 'select plan',
   },
   {
     type: 'STEP',
     title: 'Pick add-ons',
     resume: 'Add-ons help enhance your gaming experience..',
-    component: <div>step3</div>,
+    component: <Step3 />,
     buttonTag: 'add-ons',
   },
   {
@@ -39,26 +47,59 @@ const stepData: step[] = [
 ];
 
 function App() {
+  const [currentStep, setCurrentStep] = useState<number>(0);
   return (
-    <div className="App">
-      <aside>
-        <ul>
-          {stepData.map((step, index) => {
-            const idx = index + 1;
-            if (step.type !== 'END')
-              return (
-                <li>
-                  <div>{idx}</div>
-                  <p>step {idx}</p>
-                  <p>text</p>
-                </li>
-              );
-          })}
-        </ul>
-      </aside>
-      <main>
-        <Stepper steps={stepData} />
-      </main>
+    <div
+      className="w-75 mx-auto"
+      style={{ display: 'grid', placeItems: 'center', minHeight: '100vh' }}
+    >
+      <Container className="bg-white p-3 rounded-3 h-75">
+        <Row className="h-100">
+          <Col md="4" style={{ minHeight: '568px' }}>
+            <ul
+              className="vstack gap-4 p-5 h-100 rounded-3"
+              style={{
+                backgroundImage: `url(${bgV})`,
+                objectFit: 'cover',
+                backgroundPosition: 'bottom',
+                backgroundSize: 'cover',
+              }}
+            >
+              {stepData.map((step, index) => {
+                const idx = index + 1;
+                const stepStyles =
+                  index === currentStep
+                    ? 'bg-info text-primary'
+                    : 'bg-transparent text-light border border-light';
+                if (step.type !== 'END')
+                  return (
+                    <li className="hstack gap-4 p-2 " key={'step' + index}>
+                      <div
+                        className={`rounded-full rounded-circle d-flex fw-bold justify-content-center align-items-center ${stepStyles}`}
+                        style={{ height: '2.5rem', width: '2.5rem' }}
+                      >
+                        {idx}
+                      </div>
+                      <div className="vstack text-uppercase justify-content-evently">
+                        <span className="small text-white-50 ">step {idx}</span>
+                        <span className="text-light fw-bold">
+                          {step.buttonTag}
+                        </span>
+                      </div>
+                    </li>
+                  );
+              })}
+            </ul>
+          </Col>
+          <Col>
+            <Stepper
+              steps={stepData}
+              setCurrentStep={setCurrentStep}
+              currentStep={currentStep}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
